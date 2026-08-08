c35dc02072ec1e7f281f89973de65cef3c18eb11
Alex Domingo <alex.domingo.toro@vub.be>
2026-08-08 12:57:33 +0200

    add CSS class to article footer license element

--- quartz/components/ArticleFooter.tsx
+++ quartz/components/ArticleFooter.tsx
@@ -5,11 +5,11 @@ export default (() => {
   const ArticleFooter: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
     const year = new Date().getFullYear()
     return (
-      <p>
+      <p class="article-license">
         Article written by Alex Domingo.
         Licensed under <a href="https://creativecommons.org/licenses/by/4.0/">CC BY 4.0</a>
-        <img src="https://mirrors.creativecommons.org/presskit/icons/cc.svg" alt="" style="max-width: 1em;max-height:1em;margin-left: .2em;" />
-        <img src="https://mirrors.creativecommons.org/presskit/icons/by.svg" alt="" style="max-width: 1em;max-height:1em;margin-left: .2em;" />
+        <img src="https://mirrors.creativecommons.org/presskit/icons/cc.svg" alt="" style="max-width: 1em; max-height: 1em; margin-left: .2em;" />
+        <img src="https://mirrors.creativecommons.org/presskit/icons/by.svg" alt="" style="max-width: 1em; max-height: 1em; margin-left: .2em;" />
       </p>
     )
   }
